88bfb16c0f5625362760323a98c663e9781d9884
adielsimantov <adielsimantov@gmail.com>
2025-03-07 16:46:32 +0200

    add get counter

--- counter-service.py
+++ counter-service.py
@@ -2,14 +2,21 @@
 from flask import Flask, request, request_started
 
 app = Flask(__name__)
-counter = 0
-@app.route('/', methods=["POST", "GET"])
+post_counter = 0
+get_counter = 0
+@app.route('/add', methods=["POST", "GET"])
 def index():
-    global counter
+    global post_counter, get_counter 
     if request.method == "POST":
-        counter+=1
+        post_counter+=1
         return "Hmm, Plus 1 please "
-    else:
-        return str(f"Our counter is: {counter} ")
+    elif request.method == "GET":
+        get_counter+=1
+        return "Hmm, Plus 1 please "
+
+@app.route('/show', methods=["GET"])
+def show_counter():
+ return str(f"GET counter has {get_counter} and POST counter has {post_counter}")
+
 if __name__ == '__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
